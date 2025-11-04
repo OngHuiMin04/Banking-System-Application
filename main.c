7fe63056de814254cf16ff04b880e3f48a82c2d6
Amanda Ong <amandaong0427@gmail.com>
2025-11-04 10:56:53 +0800

    Initial commit -added header file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
+
+
